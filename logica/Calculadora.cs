eea5f300243323c9cc113051dd634b062595a5c4
Kauane Santos <kauanesantos.coding@gmail.com>
2024-11-14 12:41:43 -0300

    feat: create exercicio proposto 01

---
+++ logica/Calculadora.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logica
+{
+    public class Calculadora
+    {
+        public static void Soma(int a, int b)
+        {
+            try
+            {
+                Console.WriteLine($"A soma dos numeros é: {a + b}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void AreaCirculo(double r)
+        {
+            try
+            {
+                if (r < 0)
+                {
+                    throw new Exception("O raio não pode ser negativo");
+                }
+                Console.WriteLine($"A área do circulo é: {Math.Round(Math.PI * Math.Pow(r, 2), 4)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void DiferencaProduto(int a, int b, int c, int d)
+        {
+            try
+            {
+                Console.WriteLine($"A diferença do produto é: {(a * b) - (c * d)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void Salario(int num, int hrs, double valorHora)
+        {
+            try
+            {
+                double salary = hrs * valorHora;
+
+                Console.WriteLine($"Number: {num}");
+                Console.WriteLine($"Salary: {Math.Round(salary, 2)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CalcularValorCompra()
+        {
+            double total = 0;
+            string continuar = "s";
+
+            while (continuar.ToLower() == "s")
+            {
+                try
+                {
+                    Console.Write("\nDigite o código da peça: ");
+                    int cod = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite o número de peças: ");
+                    int qte = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Digite o valor unitário da peça: ");
+                    double preco = Convert.ToDouble(Console.ReadLine());
+
+                    total += qte * preco;
+
+                    Console.Write("\nDeseja adicionar outra peça? (s/n): ");
+                    continuar = Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("\nErro: " + e.Message);
+                }
+            }
+
+            Console.WriteLine($"\n---> VALOR A PAGAR: R$ {total:F2}");
+        }
+
+        public void CalcularAreas(double A, double B, double C)
+        {
+            double areaTriangulo = A * C / 2;
+            double areaCirculo = 3.14159 * Math.Pow(C, 2);
+            double areaTrapezio = (A + B * C) / 2;
+            double areaQuadrado = Math.Pow(B, 2);
+            double areaRetangulo = A * B;
+
+            Console.WriteLine($"TRIANGULO: {Math.Round(areaTriangulo, 3)}");
+            Console.WriteLine($"CIRCULO: {Math.Round(areaCirculo, 3)}");
+            Console.WriteLine($"TRAPEZIO: {Math.Round(areaTrapezio, 3)}");
+            Console.WriteLine($"QUADRADO: {Math.Round(areaQuadrado, 3)}");
+            Console.WriteLine($"RETANGULO: {Math.Round(areaRetangulo, 3)}");
+        }
+
+    }
+}
---
+++ logica/Program.cs
@@ -0,0 +1,122 @@
+﻿using logica;
+
+
+int a, b, c, d, ex, num, hrs;
+double valorHora, A, B, C;
+Calculadora calc = new();
+
+Console.WriteLine("\n---------------- Menu de Exercícios ---------------- ");
+Console.WriteLine("\n1 - Soma");
+Console.WriteLine("2 - Área do Circulo");
+Console.WriteLine("3 - Diferença do Produto");
+Console.WriteLine("4 - Salário");
+Console.WriteLine("5 - Valor da Compra");
+Console.WriteLine("6 - Calcular Áreas");
+
+Console.WriteLine("\nDigite o Número do Exercício Desejado: ");
+ex = Convert.ToInt32(Console.ReadLine());
+switch (ex)
+{
+    case 1:
+        Console.WriteLine("\nExercício 01");
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine("Digite Apenas Números Inteiros ");
+
+        Console.Write("Digite o Primeiro Número: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o Segundo Número: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Calculadora.Soma(a, b);
+        break;
+    case 2:
+        double r;
+        Console.WriteLine("\nExercício 02");
+        Console.WriteLine("---------------------------------------------");
+        Console.Write("Digite o Raio do Circulo: ");
+        r = Convert.ToDouble(Console.ReadLine());
+
+        calc.AreaCirculo(r);
+        break;
+    case 3:
+        Console.WriteLine("\nExercício 03");
+        Console.WriteLine("---------------------------------------------");
+        Console.Write("Digite o Primeiro Número: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o Segundo Número: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o Terceiro Número: ");
+        c = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o Quarto Número: ");
+        d = Convert.ToInt32(Console.ReadLine());
+
+        calc.DiferencaProduto(a, b, c, d);
+        break;
+    case 4:
+        Console.WriteLine("\nExercício 04");
+        Console.WriteLine("---------------------------------------------");
+
+        Console.Write("Digite o Número do Funcionário: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o Número de Horas Trabalhadas: ");
+        hrs = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o Valor da Hora: ");
+        valorHora = Convert.ToDouble(Console.ReadLine());
+
+        calc.Salario(num, hrs, valorHora);
+        break;
+    case 5:
+        Console.WriteLine("\nExercício 05");
+        Console.WriteLine("---------------------------------------------");
+
+
+        int option;
+        do
+        {
+            Console.WriteLine("\n---------------- Menu de Opções ---------------- ");
+            Console.WriteLine("\n1 - Calcular valor da compra de várias peças");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha uma opção: ");
+            option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    calc.CalcularValorCompra();
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida! Tente novamente.");
+                    break;
+            }
+
+        } while (option != 0);
+        break;
+    case 6:
+        Console.WriteLine("\nExercício 06");
+        Console.WriteLine("---------------------------------------------");
+
+        Console.Write("Digite um valor para A: ");
+        A = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite um valor para B: ");
+        B = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite um valor para C: ");
+        C = Convert.ToDouble(Console.ReadLine());
+
+        calc.CalcularAreas(A, B, C);
+        break;
+
+    default:
+        Console.WriteLine("Opção Inválida");
+        break;
+}
+
